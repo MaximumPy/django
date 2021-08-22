72ca8a06289cf21bcbacb2081828e1e3a7cf3713
maksimseliverstov <m.a.seliwerstov@yandex.ru>
2021-08-22 15:55:45 +0500

    создана страница html

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
-from django.template import  Context, Template
+from django.template import Context, Template
 from django.shortcuts import render
 
 def hello (request):
-    t = Template('hello, {{ name }} !')
     c = Context({'name': 'Maxim'})
-    return HttpResponse (t.render(c))
+    return render(request, 'core/hello.html', {'name': request.GET['name']})
