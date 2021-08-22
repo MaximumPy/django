1f6b0ef70eac5e6893eebcf0843f058f097f6117
maksimseliverstov <m.a.seliwerstov@yandex.ru>
2021-08-22 18:33:52 +0500

    пробная работа с базой данных

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import Context, Template
 from django.shortcuts import render
+import core.models
 
 def hello (request):
+    students = core.models.Students.objects.all()
     c = Context({'name': 'Maxim'})
-    return render(request, 'core/hello.html', {'name': request.GET['name']})
+    return render(request, 'core/hello.html', {'object_list': students})
--- project/urls.py
+++ project/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os.path
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import  settings
 import core.views
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static('node_modules', document_root=os.path.join(settings.BASE_DIR, 'node_modules'))
