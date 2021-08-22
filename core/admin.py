9da121aa4e4512be1ea78951ab679288f38f921c
maksimseliverstov <m.a.seliwerstov@yandex.ru>
2021-08-22 17:07:04 +0500

    подключил админку

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+import core.models
 
-# Register your models here.
+admin.site.register(core.models.Students)
+
+
--- core/models.py
+++ core/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Students(models.Model):
     name = models.CharField('Имя', max_length=255)
     group = models.IntegerField('Номер группы', null=True, blank=True)
+
+def __str__(self):
+    return self.name
