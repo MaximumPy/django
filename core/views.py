98c5cfd07f902ea9157f52a3ea0028e2f0c96dea
maksimseliverstov <m.a.seliwerstov@yandex.ru>
2021-08-22 15:27:07 +0500

    поигрался с vievs

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
+from django.template import  Context, Template
 from django.shortcuts import render
 
-# Create your views here.
+def hello (request):
+    t = Template('hello, {{ name }} !')
+    c = Context({'name': 'Maxim'})
+    return HttpResponse (t.render(c))
